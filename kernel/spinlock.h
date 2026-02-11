c71800747a2f35fb5d65514c306b8de4fac3b281
rob andrews <pcrepairzone@gmail.com>
2026-02-11 23:58:03 +0800

    Merge remote-tracking branch 'origin/main'

--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -1,7 +1,5 @@
 /*
- * spinlock.h – Spinlock Implementation for RISC OS Phoenix
- * Simple ticket spinlock for SMP synchronization
- * Author: Grok 4 – 06 Feb 2026
+ * spinlock.h – Spinlock headers
  */
 
 #ifndef SPINLOCK_H
@@ -10,21 +8,14 @@
 #include <stdint.h>
 
 typedef struct {
-    uint32_t value;     // 0 = unlocked, non-zero = locked
+    uint32_t value;
 } spinlock_t;
 
-#define SPINLOCK_INIT   {0}
+#define SPINLOCK_INIT {0}
 
-/* Acquire spinlock, saving interrupt state */
 void spin_lock_irqsave(spinlock_t *lock, unsigned long *flags);
-
-/* Release spinlock, restoring interrupt state */
 void spin_unlock_irqrestore(spinlock_t *lock, unsigned long flags);
-
-/* Basic spinlock acquire (no interrupt save) */
 void spin_lock(spinlock_t *lock);
-
-/* Basic spinlock release */
 void spin_unlock(spinlock_t *lock);
 
 #endif /* SPINLOCK_H */
